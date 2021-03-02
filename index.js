52182b6ff00e0307cca3b12338249a2de4a12eaf
Azim-Ahmed <azimuahmed13@gmail.com>
2021-03-02 09:37:41 +0600

    environmental variable setup done

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config()
-const port = 4000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+const port = 4000;
 
-const uri = "mongodb+srv://AfricaFinex:1234567890@cluster0.02jxk.mongodb.net/stableCoin?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DBNAME}:${process.env.DBPASS}@cluster0.02jxk.mongodb.net/${process.env.DBCOLLECTION}?retryWrites=true&w=majority`;
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const coinCollection = client.db("stableCoin").collection("coinCollection");
+  const sellCoinCollection = client
+    .db("stableCoin")
+    .collection("sellCoinCollection");
 
+  //sellerdata
 
+  app.post("/buyerdata", (req, res) => {
+    const singlebuyerdata = req.body;
+    console.log(singlebuyerdata);
+    coinCollection.insertOne(singlebuyerdata).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const coinCollection = client.db("stableCoin").collection("coinCollection");
-    const sellCoinCollection = client.db("stableCoin").collection("sellCoinCollection");
+  app.post("/sellerdata", (req, res) => {
+    const singlesellerdata = req.body;
+    console.log(singlesellerdata);
+    sellCoinCollection.insertOne(singlesellerdata).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
 
-
-
-    //sellerdata
-
-    app.post('/buyerdata', (req, res) => {
-        const singlebuyerdata = req.body;
-        console.log(singlebuyerdata);
-        coinCollection.insertOne(singlebuyerdata)
-            .then(result => {
-                res.send(result.insertedCount > 0)
-            })
-    })
-
-    app.post('/sellerdata', (req, res) => {
-        const singlesellerdata = req.body;
-        console.log(singlesellerdata);
-        sellCoinCollection.insertOne(singlesellerdata)
-            .then(result => {
-                res.send(result.insertedCount > 0)
-            })
-    })
-
-
-    console.log("database connected");
-
+  console.log("database connected");
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello !')
-})
+app.get("/", (req, res) => {
+  res.send("Hello !");
+});
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`we are opened at ${port}`)
-})
+  console.log(`we are opened at ${port}`);
+});
